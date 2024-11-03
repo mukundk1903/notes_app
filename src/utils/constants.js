f9cd61a91a1437e691c51d84694454dda12862f8
mukundk1903 <mukundk712@gmail.com>
2024-11-03 17:17:43 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'http://localhost:8000'
-export const BASE_URL = 'http://backend-notes-app-two.vercel.app'
+export const BASE_URL = 'https://backend-notes-app-two.vercel.app'
